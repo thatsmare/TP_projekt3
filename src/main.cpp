62d20bc69495fc5fefa484655d8ef57fbf46b36d
thatsmare <mpenkowska0@gmail.com>
2024-05-17 18:56:25 +0200

    no audio

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <pybind11/pybind11.h>
 #include <matplot/matplot.h>
 #include <set>
-#include <AudioFile.h>
 #include <cmath>
+
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
@@ -22,46 +22,6 @@ int multiply(int i, int j){
     return 0;
 }
 
-namespace audio
-{
-    void createAudiofile();
-}
-
-namespace audio
-{
-    void createAudioFile(){
-        std::cout << "creating an audio file" << std::endl;
-
-        AudioFile<float> aFile;
-        aFile.setNumChannels (2);
-        aFile.setNumSamplesPerChannel (44100);
-
-           //---------------------------------------------------------------
-        // 2. Create some variables to help us generate a sine wave
-        
-        const float sampleRate = 44100.f;
-        const float frequencyInHz = 440.f;
-        
-        //---------------------------------------------------------------
-        // 3. Write the samples to the AudioFile sample buffer
-        
-        for (int i = 0; i < aFile.getNumSamplesPerChannel(); i++)
-        {
-            for (int channel = 0; channel < aFile.getNumChannels(); channel++)
-            {
-                aFile.samples[channel][i] = sin ((static_cast<float> (i) / sampleRate) * frequencyInHz * 2.f * (float)M_PI);
-            }
-        }
-        
-        //---------------------------------------------------------------
-        // 4. Save the AudioFile
-        
-        std::string filePath = "sine-wave.wav"; // change this to somewhere useful for you
-        a.save ("sine-wave.wav", AudioFileFormat::Wave);
-
-
-    }
-}
 
 namespace py = pybind11;
 
